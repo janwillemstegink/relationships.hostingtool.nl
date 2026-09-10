bff1d0842e70394c5146df7db4260b9bba6cd00c
Jan Willem Stegink <techdesk@hostfusion.nl>
2026-09-10 08:32:21 +0200

    Add files via upload

--- 03_domain_objects.sql
+++ 03_domain_objects.sql
@@ -51,8 +51,7 @@ CREATE TABLE IF NOT EXISTS domains (
     domain_source_handle TEXT,
     domain_ascii_name VARCHAR(511) NOT NULL,
     domain_unicode_name VARCHAR(511) NOT NULL,
-    domain_registration_depth SMALLINT
-        CHECK (domain_registration_depth >= 0),
+    domain_registration_depth SMALLINT,
     domain_statuses TEXT[],
 	domain_policy_statuses TEXT[],
 	domain_dns_state TEXT[],
